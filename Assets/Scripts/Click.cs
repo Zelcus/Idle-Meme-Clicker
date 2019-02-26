45f04d94e50187c8e101d028de74726bc3c26b57
Zelcus <zelcus@hotmail.com>
2019-02-26 12:39:12 +0100

    Start
    All of the basic mechanics + my boy Bigweld

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+
+    public AudioClip bruhClip;
+    public AudioSource bruhSource;
+    public UnityEngine.UI.Text memeDisplay;
+    public int memes = 0;
+    public int memesPC = 1;
+
+    private void Start()
+    {
+        bruhSource.clip = bruhClip;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        memeDisplay.text = "mEmES: " + memes; 
+	}
+
+    public void Clicked()
+    {
+        bruhSource.Play();
+        memes += memesPC;
+        
+        
+        
+    }
+}
